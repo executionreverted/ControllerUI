624653a6f3fb4fd818de68d4c41f0f0a682ff1e2
executionreverted <canersevince3@gmail.com>
2021-11-28 17:40:36 +0300

    init

---
+++ pages/index.tsx
@@ -0,0 +1,166 @@
+import { useWeb3React } from "@web3-react/core";
+import Head from "next/head";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Account from "../components/Account";
+import CustomKnob from "../components/CustomKnob";
+import ETHBalance from "../components/ETHBalance";
+import TokenBalance from "../components/TokenBalance";
+import { CONTRACT_ABI } from "../contracts/ABI";
+import useContract from "../hooks/useContract";
+import useEagerConnect from "../hooks/useEagerConnect";
+import { CONTRACT_ADDRESS } from "../util";
+
+const DAI_TOKEN_ADDRESS = "0x6b175474e89094c44da98b954eedeac495271d0f";
+
+function Home() {
+  const { account, library } = useWeb3React();
+
+  const triedToEagerConnect = useEagerConnect();
+
+  const isConnected = typeof account === "string" && !!library;
+
+  const [activeParams, setParamActive] = useState({
+    0: false,
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+    6: false,
+    7: false
+  })
+  const [params, setParams] = useState(null)
+  const [section, setSection] = useState(null)
+  const knobs = [0, 1, 2, 3, 4, 5, 6, 7]
+
+  const contract = useContract(CONTRACT_ADDRESS, CONTRACT_ABI)
+
+  useEffect(() => {
+    if (contract && isConnected) {
+      if (!params) {
+        contract.getAllParamStruct().then(res => {
+          const parsed = []
+          for (let index = 0; index < 8; index++) {
+            parsed.push({
+              value: parseInt(res[index].value),
+              maxVal: parseInt(res[index].maxVal),
+              minVal: parseInt(res[index].minVal)
+            })
+          }
+          setParams(parsed)
+          console.log(parsed);
+        })
+
+        contract._section().then(res => {
+          setSection(res)
+        })
+      }
+    }
+  }, [isConnected, contract])
+
+  const save = async () => {
+
+    /*
+    struct ParamChange {
+        uint id;
+        uint value;
+    }
+    */
+
+    let payload = []
+    for (let index = 0; index < 8; index++) {
+      if (activeParams[index]) {
+        payload.push({
+          id: index,
+          value: parseInt(params[index].value)
+        });
+      }
+    }
+    console.log(payload);
+
+    const tx = await contract.changeParameterMulti(payload)
+    await tx.wait(2)
+    setParamActive({
+      0: false,
+      1: false,
+      2: false,
+      3: false,
+      4: false,
+      5: false,
+      6: false,
+      7: false
+    })
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>next-web3-boilerplate</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        {isConnected && params && (
+          <><div className="knobs-grid">
+            {
+              knobs.map(knob => <div key={knob} className="knob-">
+                <button onClick={() => {
+                  if (!activeParams[knob]) {
+                    let totalEnabled = 0;
+                    for (let index = 0; index < 7; index++) {
+                      if (activeParams[index]) totalEnabled++;
+                    }
+                    if (totalEnabled >= parseInt(section.numParams)) {
+                      console.log("Max. params reached.");
+                      return
+                    }
+                  }
+
+                  setParamActive(prev => {
+                    const newParams = Object.assign({}, prev)
+                    newParams[knob] = !newParams[knob]
+                    return newParams
+                  })
+                }} className={`${activeParams[knob] ? `toggle` : `toggle inactive`}`}>
+                  Toggle
+                </button>
+                <CustomKnob initialValue={params[knob]?.value}
+                  max={parseInt(params[knob]?.maxVal)}
+                  min={parseInt(params[knob]?.minVal)}
+                  disabled={!activeParams[knob]} onChange={(e) => {
+
+                    setParams(prev => {
+                      console.log(prev);
+                      const newParams = Object.assign({}, prev)
+                      newParams[knob].value = e
+                      return newParams
+                    })
+                  }} />
+              </div>)
+            }
+          </div>
+            <div>
+              <button onClick={save} className="toggle mt-5">
+                SAVE NEW PARAMETERS
+              </button>
+            </div>
+          </>
+        )}
+      </main>
+
+      <style jsx>{`
+        nav {
+          display: flex;
+          justify-content: space-between;
+        }
+
+        main {
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default Home;
