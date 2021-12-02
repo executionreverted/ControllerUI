7cd31c70e1a090eb0d190bb2eded7858466dce38
executionreverted <canersevince3@gmail.com>
2021-12-02 13:00:58 +0300

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,25 +19,81 @@ export default function HomePage() {
             switchChain()
         }
     }
-    
+
     return <>
         <Navbar rightChain={chainId && chainId == APP_CHAIN_ID} isConnected={isConnected} connect={connect} />
         <div className="container">
             <div className="home">
                 <h1>
-                    Lorem, ipsum dolor.
+                    Concept
                 </h1>
                 <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam labore voluptatum eum quasi! Dolorum aperiam excepturi ducimus assumenda voluptatum praesentium quasi, amet fugiat suscipit neque exercitationem modi fugit earum odit ratione? Voluptate sunt fuga voluptates possimus doloribus rem iusto nemo quia, recusandae eveniet commodi esse vitae omnis animi doloremque officiis repudiandae qui illum nihil. Vero, facilis eius facere nam error excepturi saepe modi mollitia similique labore quibusdam incidunt ex ab explicabo accusantium rem animi sunt distinctio quia voluptate ipsam dolorum beatae, natus repudiandae. Ad porro quibusdam odit assumenda est iste, placeat, animi libero repudiandae sunt, quod possimus dignissimos in ut?
+                    Smart contract controlled internet of things and sounds and images and light and voltage and bits and bytes; to be used in time based interactive compositions, permanent installations, spatial design, game mechanics, live performances, having an immutable permanent record of events.
+                </p>
+                <p>
+                    A smart contract holds “n” amount of parameters which has a min/max integer range.
+                </p>
+                <p>
+                    <strong>
+                        Example:
+                    </strong>
+                </p>
+                <p className="code">
+                    {`
+                    struct Param { 
+                        uint id;
+                        uint minVal;
+                        uint maxVal;
+                        address lastAddress;
+                        uint lastChange;
+                        uint value;
+               }`}
+                </p>
+                <p>
+                    During the performance/installation, wallets can connect to this website and edit any parameters they want.
+
+                    A python script reads the necessary parameters and variables in real time and converts them to “Open Sound Control” messages. Messages are broadcasted to every computer in the local network.
+                </p>
+                <p className="code">
+                    {`
+                    /time 329754
+                    `}
+                </p>
+
+                <p className="code">
+                    {`
+                    /param [2, 479, '0x5A135de1dB9A1eDc434e661c510B51FC199Fc728', 27033]
+                    `}
+                </p>
+
+                <p className="code">
+                    {`
+                    /section [1, 1638124971, 8, '0xFaDdfE36a6677768A5bF8b3593253D2D38b7C702']
+                    `}
+                </p>
+                <p>
+                    Any software that can read OSC can parse the incoming data and control anything. Lights in a room, pitch of sine waves, relay controlled electronics, fog machine, modular synth, ram data of a nes game…
+                </p>
+                <p>
+                    Interaction of users can be gamified in various ways and use an erc20 token as a utility currency.
                 </p>
             </div>
             <div id="#about" className="about">
                 <h1>
-                    Lorem, ipsum dolor sit About.
+                    Use case
                 </h1>
                 <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam labore voluptatum eum quasi! Dolorum aperiam excepturi ducimus assumenda voluptatum praesentium quasi, amet fugiat suscipit neque exercitationem modi fugit earum odit ratione? Voluptate sunt fuga voluptates possimus doloribus rem iusto nemo quia, recusandae eveniet commodi esse vitae omnis animi doloremque officiis repudiandae qui illum nihil. Vero, facilis eius facere nam error excepturi saepe modi mollitia similique labore quibusdam incidunt ex ab explicabo accusantium rem animi sunt distinctio quia voluptate ipsam dolorum beatae, natus repudiandae. Ad porro quibusdam odit assumenda est iste, placeat, animi libero repudiandae sunt, quod possimus dignissimos in ut?
-                </p>
+                    Music for smart contract controlled electronics + visuals and instruments
+                    Duration: 20-30 minutes
+                    <br />
+
+                    8 parameters in contract.
+                    <br />
+                    <br />
+                    Supercollider reads the data for generating sounds / musical phrases - touchdesigner generate real time visuals. Instruments play alongside.
+                    <br />
+                    <br />
+                    There is a section counter in the smart contract, and every 3 minutes it can be changed by a wallet. While this user changes the section, they can determine how many parameters a wallet can edit at once.                </p>
             </div>
             <div id="#team" className="team">
                 <h1>
